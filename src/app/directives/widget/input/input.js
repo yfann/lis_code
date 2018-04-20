dd986270289253fb073143ad0a7e0cc389c40d6d
Fan <fan.yu@philips.com>
2018-04-20 11:10:45 +0800

    update

--- src/app/directives/widget/input/input.js
+++ src/app/directives/widget/input/input.js
@@ -7,6 +7,9 @@ angular.module('uiDirect')
             },
             replace: true,
             templateUrl: 'app/directives/widget/input/input.html',
+            link:function($scope, elem, attr, ctrl){
+
+            },
             controller: function ($scope, $element, $attrs) {
 
             }
