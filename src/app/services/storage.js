b9b00a3d8764e5621f30c77e8ebb2ba7d1546e62
Fan <fan.yu@philips.com>
2018-06-12 22:24:03 +0800

    update

--- src/app/services/storage.js
+++ src/app/services/storage.js
@@ -1,32 +1,38 @@
 angular.module('commonService').
-    service('storage', ['$localStorage', function ($localStorage) {
+    service('storage', ['$localStorage','$cookies', function ($localStorage,$cookies) {
 
         return {
             callback: null,
             setTokenAndUser: function (token, user) {
-                $localStorage.token = token;
-                $localStorage.user = user;
+                // $localStorage.token = token;
+                // $localStorage.user = user;
+                $cookies.put('token',token);
+                $cookies.putObject('user',user);
                 if (this.callback) {
                     this.callback(user, this.isAdmin());
                 }
             },
             logout: function () {
-                $localStorage.token = null;
-                $localStorage.user = null;
+                // $localStorage.token = null;
+                // $localStorage.user = null;
+                $cookies.remove("token");
+                $cookies.remove("user");
             },
             getUser: function () {
-                return $localStorage.user;
+                //return $localStorage.user;
+                return $cookies.getObject("user");
             },
             isLogin: function () {
-                if ($localStorage.token) {
+                if ($cookies.get("token")) {
                     return true;
                 } else {
                     return false;
                 }
             },
             isAdmin: function () {
-                if ($localStorage.user) {
-                    if ($localStorage.user.emCode && $localStorage.user.emCode.toLowerCase() == 'admin') {
+                var user=$cookies.getObject("user");
+                if (user) {
+                    if (user.emCode && user.emCode.toLowerCase() == 'admin') {
                         return true;
                     }
                 }
